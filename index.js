8ddc902ed58989a3e1717dfc4ba555fa88ac1759
Hassan Mujtaba <mhassanmujtaba2007@gmail.com>
2026-08-18 23:48:43 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+const path=require('path');
+const userRoute = require('./route/user');
+const mongoose = require('mongoose');
+const cookieParser = require("cookie-parser");
+const { checkForAuthenticationCookie } = require('./middlewares/authentication');
+const blogRoute = require("./route/blog");
+const blog = require('./models/blog');
+
+app.set('view engine','ejs');
+app.set('views',path.resolve('./views'));
+
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve('./public')));
+
+mongoose.connect('mongodb://localhost:27017/blogify').then(console.log("MongoDb connnected!"));
+
+app.get('/', async (req,res)=>{
+    const allBlogs = await blog.find({});
+    res.render('home',{
+        user: req.user,
+        blogs: allBlogs,
+    });
+})
+
+app.use('/user',userRoute);
+app.use('/blog', blogRoute);
+
+app.listen(PORT, ()=> console.log(`Server Started at PORT: ${PORT}`));
---
+++ route/blog.js
@@ -0,0 +1,58 @@
+const {Router} = require('express');
+const User = require('../models/user');
+const router = Router();
+const multer = require("multer");
+const path = require('path');
+const blog = require('../models/blog');
+const Comment = require('../models/comment');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(`./public/uploads/`));
+  },
+  filename: function (req, file, cb) {
+    const fileName = `${Date.now()}-${file.originalname}`;
+    cb(null,fileName);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.get('/add-new',(req,res)=>{
+    return res.render('addBlog',{
+        user:req.user
+    });
+})
+
+router.post('/',upload.single('coverImage'), async (req,res)=>{
+    const {title , body} = req.body;
+   const Blog = await blog.create({
+        body,
+        title,
+        createdBy : req.user._id, 
+        coverImageURL : `/uploads/${req.file.filename}`
+    })
+    return res.redirect(`/Blog/${Blog._id}`);
+})
+
+router.get('/:id', async (req,res)=>{
+    const getBlog = await blog.findById(req.params.id).populate("createdBy");
+    const comments = await Comment.find({blogId : req.params.id}).populate("createdBy");
+    console.log("comments",comments)
+    return res.render('blog',{
+      user : req.user,
+      getBlog,
+      comments,
+    })
+})
+
+router.post('/comment/:blogId', async (req,res)=>{
+  const comment = await Comment.create({
+    content: req.body.content,
+    blogId : req.params.blogId,
+    createdBy: req.user._id
+  })
+  return res.redirect(`/blog/${req.params.blogId}`);
+})
+
+module.exports = router;
